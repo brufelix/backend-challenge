7a3aa29e4f9734dea203b6de0ccc7d0f57298502
brufelix <brunofelix.ml@gmail.com>
2024-08-31 20:36:05 -0300

    feat: configuration connection with database

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,46 @@
-import 'module-alias/register';
-
+import { DataSource } from 'typeorm';
 import { envs } from '@/constants/envs';
 import express, { Application } from 'express';
+import { ormConfigOptions } from './config/orm-config-options';
 
-const port = envs.port;
-const app: Application = express();
+import 'module-alias/register';
 
-app.use(express.json());
+class Server {
+  private port: number;
+  private app: Application;
+  private dataSource: DataSource;
 
-app.listen(port, () => {
-  console.log(`Server running on port ${port}.`);
-});
+  constructor() {
+    this.app = express();
+    this.port = Number(envs.port);
+    this.dataSource = new DataSource(ormConfigOptions);
+
+    this.configureMiddleware();
+  }
+
+  private configureMiddleware(): void {
+    this.app.use(express.json());
+  }
+
+  private async initializeDataSource(): Promise<void> {
+    try {
+      await this.dataSource.initialize();
+      console.info('Data Source has been initialized.');
+    } catch (err) {
+      console.error(`Data Source initialization error: ${err}`);
+
+      process.exit(1);
+    }
+  }
+
+  public async start(): Promise<void> {
+    await this.initializeDataSource();
+
+    this.app.listen(this.port, () => {
+      console.info(`Server running on port ${this.port}.`);
+    });
+  }
+}
+
+const server = new Server();
+server.start();
