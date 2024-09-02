00f56cceb7cac5a26946122440a9e13d4b4acd71
brufelix <brunofelix.ml@gmail.com>
2024-09-02 16:06:45 -0300

    feat: add auth service

---
+++ src/services/auth.service.ts
@@ -0,0 +1,44 @@
+import { Hash } from '@/helpers/hash.helper';
+import { User } from '@/entities/user.entity';
+import { JwtToken } from '@/helpers/jwt-token.helper';
+import { LoginDto } from '@/controllers/auth/dto/login.dto';
+import { UsersRepository } from '@/repositories/users.repository';
+import { BadRequestError } from '@/helpers/errors/bad-request-error';
+import { UserLoggedResponse } from '@/@types/user-logged-response.type';
+
+export class AuthService {
+  constructor(public usersRepository = new UsersRepository()) {}
+
+  async login({ email, password }: LoginDto): Promise<UserLoggedResponse> {
+    const validUser = await this.validateUser(email, password);
+
+    const { id, name } = validUser;
+    const payload = { id, name, email, password };
+
+    const access_token = JwtToken.generateToken(payload);
+    return { data: validUser, access_token };
+  }
+
+  private async validateUser(email: string, password: string): Promise<User> {
+    const user = await this.usersRepository.findOne({
+      where: { email },
+    });
+
+    if (!user) {
+      throw new BadRequestError('Usuário ou Senha inválida.');
+    }
+
+    const isMatch: boolean = await Hash.comparePasswords(
+      password,
+      user.password
+    );
+
+    if (!isMatch) {
+      throw new BadRequestError('Usuário ou Senha inválida.');
+    }
+
+    delete user.password;
+
+    return user;
+  }
+}
