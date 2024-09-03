c116653693753e928fb186642f3820111263969a
brufelix <brunofelix.ml@gmail.com>
2024-09-03 15:39:53 -0300

    fix: remove src of path

--- jest.config.js
+++ jest.config.js
@@ -4,7 +4,7 @@ module.exports = {
   roots: ['<rootDir>/src'],
   testMatch: ['**/__tests__/**/*.test.ts', '**/?(*.)+(spec|test).ts'],
   moduleNameMapper: {
-    '^@src/(.*)$': '<rootDir>/src/$1',
+    '^@/(.*)$': '<rootDir>/src/$1',
   },
   transform: {
     '^.+\\.(ts)$': [
