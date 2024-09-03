c2ae89a22f160cc5ca66578a014e34df1110efb0
brufelix <brunofelix.ml@gmail.com>
2024-09-02 21:36:21 -0300

    fix: remove connection of database

--- src/server.ts
+++ src/server.ts
@@ -3,19 +3,15 @@ import 'module-alias/register';
 
 import cors from 'cors';
 import router from './routes';
-import { DataSource } from 'typeorm';
 import { envs } from '@/constants/envs';
 import express, { Application } from 'express';
-import { dataSource } from './config/data-source.config';
 
 class Server {
   private port: number;
   private app: Application;
-  private dataSource: DataSource;
 
   constructor() {
     this.app = express();
-    this.dataSource = dataSource;
     this.port = Number(envs.port);
 
     this.configureMiddleware();
@@ -29,20 +25,7 @@ class Server {
     this.app.use(router);
   }
 
-  private async initializeDataSource(): Promise<void> {
-    try {
-      await this.dataSource.initialize();
-      console.info('Data Source has been initialized.');
-    } catch (err) {
-      console.error(`Data Source initialization error: ${err}`);
-
-      process.exit(1);
-    }
-  }
-
   public async start(): Promise<void> {
-    await this.initializeDataSource();
-
     this.app.listen(this.port, () => {
       console.info(`Server running on port ${this.port}.`);
     });
