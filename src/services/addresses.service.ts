d49df14c82ca15f9f8f8021d873f4cc2ecfcb850
brufelix <brunofelix.ml@gmail.com>
2024-09-02 21:35:49 -0300

    feat: add typing in return of methods

--- src/services/addresses.service.ts
+++ src/services/addresses.service.ts
@@ -1,38 +1,46 @@
+import { ILike } from 'typeorm';
+import { Address } from '@/entities/address.entity';
+import { NotFoundError } from '@/helpers/errors/not-found-error';
+import { AddressesRepository } from '@/repositories/addresses.repository';
 import { CreateAddressDto } from '@/controllers/addresses/dto/create-address.dto';
 import { ListAddressesDto } from '@/controllers/addresses/dto/list-addresses.dto';
 import { UpdateAddressDto } from '@/controllers/addresses/dto/update-address.dto';
-import { AddressesRepository } from '@/repositories/addresses.repository';
 
 export class AddressesService {
   constructor(public addressesRepository = new AddressesRepository()) {}
 
-  async insert(data: CreateAddressDto) {
+  async insert(data: CreateAddressDto): Promise<Address> {
     return this.addressesRepository.insert(data);
   }
 
-  async findById(id: string) {
-    return this.addressesRepository.findOne({
+  async findById(id: string): Promise<Address> {
+    const address = await this.addressesRepository.findOne({
       where: { id },
+      relations: ['user'],
     });
+
+    if (!address) throw new NotFoundError('Endereço não encontrado.');
+
+    return address;
   }
 
-  async list(query: ListAddressesDto) {
+  async list(query: ListAddressesDto): Promise<Address[]> {
     return this.addressesRepository.list({
-      relations: ['users'],
+      relations: ['user'],
       order: { createdAt: 'DESC' },
       where: {
-        ...(query?.state && { state: query.state }),
-        ...(query?.userId && { userIdI: query.userId }),
-        ...(query?.country && { country: query.country }),
+        ...(query?.userId && { userId: query.userId }),
+        ...(query?.state && { state: ILike(query.state) }),
+        ...(query?.country && { country: ILike(query.country) }),
       },
     });
   }
 
-  async update(id: string, data: UpdateAddressDto) {
+  async update(id: string, data: UpdateAddressDto): Promise<Address> {
     return this.addressesRepository.update(id, data);
   }
 
-  async destroy(id: string) {
-    return this.addressesRepository.destroy(id);
+  async destroy(id: string): Promise<void> {
+    await this.addressesRepository.destroy(id);
   }
 }
--- src/services/users.service.ts
+++ src/services/users.service.ts
@@ -1,17 +1,19 @@
 import { Hash } from '@/helpers/hash.helper';
 import { User } from '@/entities/user.entity';
+import { Database } from '@/config/database.config';
 import { Address } from '@/entities/address.entity';
 import { JwtToken } from '@/helpers/jwt-token.helper';
-import { dataSource } from '@/config/data-source.config';
 import { ConflictError } from '@/helpers/errors/conflict-error';
+import { NotFoundError } from '@/helpers/errors/not-found-error';
 import { UsersRepository } from '@/repositories/users.repository';
 import { CreateUserDto } from '@/controllers/users/dto/create-user.dto';
 import { UpdateUserDto } from '@/controllers/users/dto/update-user.dto';
+import { UserLoggedResponse } from '@/@types/user-logged-response.type';
 
 export class UsersService {
   constructor(public usersRepository = new UsersRepository()) {}
 
-  async insert(data: CreateUserDto) {
+  async insert(data: CreateUserDto): Promise<UserLoggedResponse> {
     const email = data.email.toLowerCase();
 
     await this.checkEmailAvailability(email);
@@ -29,20 +31,22 @@ export class UsersService {
 
     const access_token = JwtToken.generateToken(payload);
 
-    return { access_token, user };
+    return { access_token, data: user };
   }
 
-  async me(id: string) {
-    return this.usersRepository.findOne({
+  async me(id: string): Promise<User> {
+    const user = await this.usersRepository.findOne({
       where: { id },
       relations: ['addresses'],
     });
+
+    if (!user) throw new NotFoundError('Usuário não encontrado.');
+
+    return user;
   }
 
-  async update(id: string, data: UpdateUserDto) {
-    const user = await this.usersRepository.findOne({
-      where: { id },
-    });
+  async update(id: string, data: UpdateUserDto): Promise<User> {
+    const user = await this.me(id);
 
     const hasNewEmail = !!data?.email && user.email !== data.email;
     if (hasNewEmail) await this.checkEmailAvailability(data.email!);
@@ -59,16 +63,16 @@ export class UsersService {
     });
   }
 
-  async destroy(id: string) {
-    const user = await this.usersRepository.findOne({
-      where: { id },
-    });
+  async destroy(id: string): Promise<void> {
+    const user = await this.me(id);
 
     const addressesId = user.addresses.map(({ id }) => id);
 
-    await dataSource.transaction(async (transactionalEntityManager) => {
-      await transactionalEntityManager.delete(User, id);
+    const DatabaseInstance = await Database.getInstance();
+
+    DatabaseInstance.transaction(async (transactionalEntityManager) => {
       await transactionalEntityManager.delete(Address, addressesId);
+      await transactionalEntityManager.delete(User, id);
     });
   }
 
